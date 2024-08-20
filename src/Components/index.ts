40359560de3d8c4cffa77739058af2cde881b106
jgonzales <jgonzales@infosis.tech>
2024-08-19 21:35:00 -0300

    chore: FRN-010 created Badge and notification/status

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,3 +1,4 @@
 export { default as FormInput } from './FormInput/FormInput'
 export { default as Button } from './Button/Button'
 export { default as Loader } from './Loader/Loader'
+export { default as Badge } from './Badge/Badge'
--- src/Pages/Dashboard/Components/UserCard/UserCard.tsx
+++ src/Pages/Dashboard/Components/UserCard/UserCard.tsx
@@ -1,7 +1,12 @@
+import Badge from '../../../../Components/Badge/Badge'
 import styles from './UserCard.module.css'
 
 const UserCard = () => {
-  return <div></div>
+  return (
+    <div>
+      <Badge extra={{ text: 'asd' }} />
+    </div>
+  )
 }
 
 export default UserCard
